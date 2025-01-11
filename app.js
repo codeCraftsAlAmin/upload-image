d2a657ed3e9596c37e55439923a77208967211df
codeCrafts <codecraftsman12@gmail.com>
2025-01-11 20:52:35 +0600

    new changes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // requires
 const express = require("express");
+const path = require("path");
 const app = express();
 const userRouter = require("./routes/user.route");
 const cors = require("cors");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
