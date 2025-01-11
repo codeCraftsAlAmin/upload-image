95a448e2409d79a3b923f74f4fc49c7c59baba2c
codeCrafts <codecraftsman12@gmail.com>
2025-01-11 21:15:22 +0600

    new changes

--- routes/user.route.js
+++ routes/user.route.js
@@ -18,7 +18,7 @@ route.get("/register", userRegister);
 // multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./uploads");
+    cb(null, "uploads/");
   },
   filename: function (req, file, cb) {
     const name = Date.now() + "-" + file.originalname;
